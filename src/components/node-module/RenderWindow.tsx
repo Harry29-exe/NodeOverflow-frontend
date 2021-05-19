b21809f88fed9da2eb7ece63beab208f3d27e455
Kamil <wojcikakamil@gmail.com>
2021-05-19 15:15:38 +0200

    RenderWindow.tsx now works again

--- src/components/node-module/RenderWindow.tsx
+++ src/components/node-module/RenderWindow.tsx
@@ -1,89 +1,93 @@
 import React, {Component} from 'react';
 import {NodeModel} from "../../logic/node-editor/node/NodeModel";
 import {ProjectStorage} from "../../logic/node-editor/node-management/ProjectStorage";
+import {Box, Button} from "@chakra-ui/react";
 
-class Props {
-    nodes: NodeModel[];
-
-    constructor(nodes: NodeModel[]) {
-        this.nodes = nodes;
-    }
+interface Props {
+    storage: ProjectStorage
 }
 
-class RenderWindow extends Component<{ storage: ProjectStorage }, { lastUpdate: number }> {
+class RenderWindow extends Component<Props, { lastUpdate: number }> {
     private ref = React.createRef<HTMLCanvasElement>();
 
+    constructor(props: Props) {
+        super(props);
+        this.state = {lastUpdate: 0};
+    }
 
-    // constructor(props: Props) {
-    //     super(props);
-    //     this.state = {lastUpdate: 0};
-    // }
-    //
-    // componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<any>, snapshot?: any) {
-    //     if (Date.now() - this.state.lastUpdate > 1000) {
-    //         let outputNode: NodeModel | null = null;
-    //         let nodes = this.props.nodes;
-    //         for (let i = 0; i < nodes.length; i++) {
-    //             if (nodes[i].name === "Output") {
-    //                 outputNode = nodes[i];
-    //                 break;
-    //             }
-    //         }
-    //         this.setState({lastUpdate: Date.now()});
-    //         if (outputNode !== null) {
-    //             this.paintOutput(outputNode);
-    //         }
-    //     }
-    // }
-    //
-    // paintOutput(outputNode: NodeModel) {
-    //     try {
-    //         outputNode.getOutputValue(0).then(
-    //             value => this.paintCanvas(value),
-    //             reason => console.log("something went wrong")
-    //         );
-    //     } catch (e: any) {
-    //         console.log(e);
-    //     }
-    // }
-    //
-    // paintCanvas = (imageData: ImageData) => {
-    //     if (this.ref.current) {
-    //         let temp = document.createElement("canvas") as HTMLCanvasElement;
-    //         temp.width = imageData.width;
-    //         temp.height = imageData.height;
-    //         let tempCTX = temp.getContext("2d") as CanvasRenderingContext2D;
-    //         tempCTX.putImageData(imageData, 0, 0);
-    //
-    //         let canvas = this.ref.current;
-    //         let maxW = (this.ref.current.closest("div") as HTMLDivElement).clientWidth;
-    //         let maxH = (this.ref.current.closest("div") as HTMLDivElement).clientHeight;
-    //         let width: number;
-    //         let height: number;
-    //         if ((maxW / imageData.width) * imageData.height < maxH) {
-    //             width = maxW;
-    //             height = (maxW / imageData.width) * imageData.height;
-    //         } else {
-    //             height = maxH;
-    //             width = (maxH / imageData.height) * imageData.width;
-    //         }
-    //         canvas.width = maxW;
-    //         canvas.height = maxH;
-    //         let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    //         ctx.drawImage(temp, 0, 0, imageData.width, imageData.height,
-    //             (maxW - width) / 2, (maxH - height) / 2, width, height);
-    //
-    //     } else {
-    //         throw new Error("Output node has to be connected to node returning image");
-    //     }
-    //
-    //     this.setState({lastUpdate: Date.now()});
-    // }
+    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<any>, snapshot?: any) {
+        console.log("render window update");
+        if (Date.now() - this.state.lastUpdate > 1000) {
+            let outputNode: NodeModel | null = null;
+            let nodes = this.props.storage.getNodes();
+            for (let i = 0; i < nodes.length; i++) {
+                if (nodes[i].name === "Output node") {
+                    outputNode = nodes[i];
+                    break;
+                }
+            }
+            this.setState({lastUpdate: Date.now()});
+            if (outputNode !== null) {
+                this.paintOutput(outputNode);
+            }
+        }
+    }
+
+    paintOutput(outputNode: NodeModel) {
+        try {
+            outputNode.getOutputValue(0).then(
+                value => this.paintCanvas(value),
+                reason => console.log("something went wrong")
+            );
+        } catch (e: any) {
+            console.log(e);
+        }
+    }
+
+    paintCanvas = (imageData: ImageData) => {
+        console.log("painting")
+        if (this.ref.current) {
+            let temp = document.createElement("canvas") as HTMLCanvasElement;
+            temp.width = imageData.width;
+            temp.height = imageData.height;
+            let tempCTX = temp.getContext("2d") as CanvasRenderingContext2D;
+            tempCTX.putImageData(imageData, 0, 0);
+
+            let canvas = this.ref.current;
+            let maxW = (this.ref.current.closest("div") as HTMLDivElement).clientWidth;
+            let maxH = (this.ref.current.closest("div") as HTMLDivElement).clientHeight;
+            let width: number;
+            let height: number;
+            if ((maxW / imageData.width) * imageData.height < maxH) {
+                width = maxW;
+                height = (maxW / imageData.width) * imageData.height;
+            } else {
+                height = maxH;
+                width = (maxH / imageData.height) * imageData.width;
+            }
+            canvas.width = maxW;
+            canvas.height = maxH;
+            let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+            ctx.drawImage(temp, 0, 0, imageData.width, imageData.height,
+                (maxW - width) / 2, (maxH - height) / 2, width, height);
+
+        } else {
+            throw new Error("Output node has to be connected to node returning image");
+        }
+
+        this.setState({lastUpdate: Date.now()});
+    }
 
     render() {
         return (
-            <canvas ref={this.ref}/>
-
+            <>
+                <Box pos='absolute' left={0} top={0}>
+                    <Button onClick={() => this.setState({lastUpdate: 0})}>
+                        Render
+                    </Button>
+                </Box>
+                <canvas ref={this.ref}/>
+            </>
         );
     }
 }
