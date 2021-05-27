3f1509a04b2b7b22de7e9d6b6c998d9eb332e2c2
Kamil <wojcikakamil@gmail.com>
2021-05-27 15:51:35 +0200

    added addresses of ipv4 host

--- src/logic/addresses/ProjectServerAddress.ts
+++ src/logic/addresses/ProjectServerAddress.ts
@@ -1,1 +1,2 @@
-export const projectServerAddress = "http://localhost:8080"
+// export const projectServerAddress = "http://localhost:8080";
+export const projectServerAddress = "http://109.200.83.220:81";
